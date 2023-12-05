61576b18a0b82cac07cd369ea77b09e5a823b968
Anderson Couto <andersoncouto57@gmail.com>
2023-12-05 19:10:24 -0300

    dices class

--- examples/dice/dices.cpp
+++ examples/dice/dices.cpp
@@ -7,16 +7,15 @@
 #include <glm/gtx/hash.hpp>
 #include <unordered_map>
 
-namespace std {
-  template <> struct hash<Vertex> {
-    size_t operator()(Vertex const& vertex) const noexcept {
-      const std::size_t h1{std::hash<glm::vec3>()(vertex.position)};
-      const std::size_t h2{std::hash<glm::vec3>()(vertex.normal)};
-      const std::size_t h3{std::hash<glm::vec2>()(vertex.texCoord)};
-      return h1 ^ h2 ^ h3;
-    }
-  };
-}
+// Explicit specialization of std::hash for Vertex
+template <> struct std::hash<Vertex> {
+  size_t operator()(Vertex const &vertex) const noexcept {
+    auto const h1{std::hash<glm::vec3>()(vertex.position)};
+    auto const h2{std::hash<glm::vec3>()(vertex.normal)};
+    auto const h3{std::hash<glm::vec2>()(vertex.texCoord)};
+    return abcg::hashCombine(h1, h2, h3);
+  }
+};
 
 void Dices::create(int quantity){
   auto seed{std::chrono::steady_clock::now().time_since_epoch().count()};
@@ -143,25 +142,31 @@ void Dices::checkCollisions(Dice &dice){
 }
 
 void Dices::computeNormals() {
-  for (auto& vertex : m_vertices) {
-    vertex.normal = glm::zero<glm::vec3>();
+  // Clear previous vertex normals
+  for (auto &vertex : m_vertices) {
+    vertex.normal = glm::vec3(0.0f);
   }
 
-  for (const auto offset : iter::range<int>(0, m_indices.size(), 3)) {
-    Vertex& a{m_vertices.at(m_indices.at(offset + 0))};
-    Vertex& b{m_vertices.at(m_indices.at(offset + 1))};
-    Vertex& c{m_vertices.at(m_indices.at(offset + 2))};
+  // Compute face normals
+  for (auto const offset : iter::range<int>(0, m_indices.size(), 3)) {
+    // Get face vertices
+    auto &a{m_vertices.at(m_indices.at(offset + 0))};
+    auto &b{m_vertices.at(m_indices.at(offset + 1))};
+    auto &c{m_vertices.at(m_indices.at(offset + 2))};
 
-    const auto edge1{b.position - a.position};
-    const auto edge2{c.position - b.position};
-    const glm::vec3 normal{glm::cross(edge1, edge2)};
+    // Compute normal
+    auto const edge1{b.position - a.position};
+    auto const edge2{c.position - b.position};
+    auto const normal{glm::cross(edge1, edge2)};
 
+    // Accumulate on vertices
     a.normal += normal;
     b.normal += normal;
     c.normal += normal;
   }
 
-  for (auto& vertex : m_vertices) {
+  // Normalize
+  for (auto &vertex : m_vertices) {
     vertex.normal = glm::normalize(vertex.normal);
   }
 
@@ -169,33 +174,40 @@ void Dices::computeNormals() {
 }
 
 void Dices::createBuffers() {
+  // Delete previous buffers
   abcg::glDeleteBuffers(1, &m_EBO);
   abcg::glDeleteBuffers(1, &m_VBO);
+
+  // VBO
   abcg::glGenBuffers(1, &m_VBO);
   abcg::glBindBuffer(GL_ARRAY_BUFFER, m_VBO);
-  abcg::glBufferData(GL_ARRAY_BUFFER, sizeof(m_vertices[0]) * m_vertices.size(),
+  abcg::glBufferData(GL_ARRAY_BUFFER,
+                     sizeof(m_vertices.at(0)) * m_vertices.size(),
                      m_vertices.data(), GL_STATIC_DRAW);
   abcg::glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+  // EBO
   abcg::glGenBuffers(1, &m_EBO);
   abcg::glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_EBO);
   abcg::glBufferData(GL_ELEMENT_ARRAY_BUFFER,
-                     sizeof(m_indices[0]) * m_indices.size(), m_indices.data(),
-                     GL_STATIC_DRAW);
+                     sizeof(m_indices.at(0)) * m_indices.size(),
+                     m_indices.data(), GL_STATIC_DRAW);
   abcg::glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
 }
 
 void Dices::loadDiffuseTexture(std::string_view path) {
-  if (!std::filesystem::exists(path)) return;
-  
+  if (!std::filesystem::exists(path))
+    return;
+
   abcg::glDeleteTextures(1, &m_diffuseTexture);
-  m_diffuseTexture = abcg::loadOpenGLTexture( {.path = path} );
+  m_diffuseTexture = abcg::loadOpenGLTexture({.path = path});
 }
 
 void Dices::loadObj(std::string_view path, bool standardize) {
   auto const basePath{std::filesystem::path{path}.parent_path().string() + "/"};
 
   tinyobj::ObjReaderConfig readerConfig;
-  readerConfig.mtl_search_path = basePath;
+  readerConfig.mtl_search_path = basePath; // Path to material files
 
   tinyobj::ObjReader reader;
 
@@ -221,17 +233,23 @@ void Dices::loadObj(std::string_view path, bool standardize) {
   m_hasNormals = false;
   m_hasTexCoords = false;
 
+  // A key:value map with key=Vertex and value=index
   std::unordered_map<Vertex, GLuint> hash{};
 
+  // Loop over shapes
   for (auto const &shape : shapes) {
+    // Loop over indices
     for (auto const offset : iter::range(shape.mesh.indices.size())) {
+      // Access to vertex
       auto const index{shape.mesh.indices.at(offset)};
 
+      // Position
       auto const startIndex{3 * index.vertex_index};
       glm::vec3 position{attrib.vertices.at(startIndex + 0),
                          attrib.vertices.at(startIndex + 1),
                          attrib.vertices.at(startIndex + 2)};
 
+      // Normal
       glm::vec3 normal{};
       if (index.normal_index >= 0) {
         m_hasNormals = true;
@@ -241,6 +259,7 @@ void Dices::loadObj(std::string_view path, bool standardize) {
                   attrib.normals.at(normalStartIndex + 2)};
       }
 
+      // Texture coordinates
       glm::vec2 texCoord{};
       if (index.texcoord_index >= 0) {
         m_hasTexCoords = true;
@@ -252,8 +271,11 @@ void Dices::loadObj(std::string_view path, bool standardize) {
       Vertex const vertex{
           .position = position, .normal = normal, .texCoord = texCoord};
 
+      // If hash doesn't contain this vertex
       if (!hash.contains(vertex)) {
+        // Add this index (size of m_vertices)
         hash[vertex] = m_vertices.size();
+        // Add this vertex
         m_vertices.push_back(vertex);
       }
 
@@ -261,8 +283,9 @@ void Dices::loadObj(std::string_view path, bool standardize) {
     }
   }
 
+  // Use properties of first material, if available
   if (!materials.empty()) {
-    auto const &mat{materials.at(0)};
+    auto const &mat{materials.at(0)}; // First material
     m_Ka = {mat.ambient[0], mat.ambient[1], mat.ambient[2], 1};
     m_Kd = {mat.diffuse[0], mat.diffuse[1], mat.diffuse[2], 1};
     m_Ks = {mat.specular[0], mat.specular[1], mat.specular[2], 1};
@@ -271,6 +294,7 @@ void Dices::loadObj(std::string_view path, bool standardize) {
     if (!mat.diffuse_texname.empty())
       loadDiffuseTexture(basePath + mat.diffuse_texname);
   } else {
+    // Default values
     m_Ka = {0.1f, 0.1f, 0.1f, 1.0f};
     m_Kd = {0.7f, 0.7f, 0.7f, 1.0f};
     m_Ks = {1.0f, 1.0f, 1.0f, 1.0f};
@@ -290,10 +314,15 @@ void Dices::loadObj(std::string_view path, bool standardize) {
 
 void Dices::render(int numTriangles) const {
   abcg::glBindVertexArray(m_VAO);
+
   abcg::glActiveTexture(GL_TEXTURE0);
   abcg::glBindTexture(GL_TEXTURE_2D, m_diffuseTexture);
+
+  // Set minification and magnification parameters
   abcg::glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
   abcg::glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+  // Set texture wrapping parameters
   abcg::glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
   abcg::glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
 
@@ -306,12 +335,18 @@ void Dices::render(int numTriangles) const {
 }
 
 void Dices::setupVAO(GLuint program) {
+  // Release previous VAO
   abcg::glDeleteVertexArrays(1, &m_VAO);
+
+  // Create VAO
   abcg::glGenVertexArrays(1, &m_VAO);
   abcg::glBindVertexArray(m_VAO);
+
+  // Bind EBO and VBO
   abcg::glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_EBO);
   abcg::glBindBuffer(GL_ARRAY_BUFFER, m_VBO);
 
+  // Bind vertex attributes
   auto const positionAttribute{
       abcg::glGetAttribLocation(program, "inPosition")};
   if (positionAttribute >= 0) {
@@ -339,11 +374,15 @@ void Dices::setupVAO(GLuint program) {
                                 reinterpret_cast<void *>(offset));
   }
 
+  // End of binding
   abcg::glBindBuffer(GL_ARRAY_BUFFER, 0);
   abcg::glBindVertexArray(0);
 }
 
 void Dices::standardize() {
+  // Center to origin and normalize largest bound to [-1, 1]
+
+  // Get bounds
   glm::vec3 max(std::numeric_limits<float>::lowest());
   glm::vec3 min(std::numeric_limits<float>::max());
   for (auto const &vertex : m_vertices) {
@@ -351,6 +390,7 @@ void Dices::standardize() {
     min = glm::min(min, vertex.position);
   }
 
+  // Center and scale
   auto const center{(min + max) / 2.0f};
   auto const scaling{2.0f / glm::length(max - min)};
   for (auto &vertex : m_vertices) {
